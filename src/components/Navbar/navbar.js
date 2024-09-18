2d2b7319502ed5e6b53f05290af751abb5ab2187
David <davidtienh@gmail.com>
2024-09-18 15:17:17 -0400

    Fixing git, added src files

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -2,16 +2,15 @@ import React from "react";
 import './navbar.css';
 import {Link } from 'react-scroll';
 import "./navbar.css";
-import contactImage from '../../assets/contact.png';
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="desktopMenu"> 
-                <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Home</Link>
-                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">About me</Link>
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">About me</Link>
                 <Link className="desktopMenuListItem">Projects</Link>
             </div>
-            <button className="desktopMenuButton"> <img src={contactImage} alt="" className="desktopMenuImage" /> contact </button>
+            <button className="desktopMenuButton"> <img src="" alt="" className="desktopMenuImage" /> contact </button>
         </nav>
     );
 }
